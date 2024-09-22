8d0cd7ad0badbeb56b82f7ce60758a210bf89546
Henrique Patricio <henriquepworking@gmail.com>
2024-09-22 13:57:10 -0300

    feat: implementation function info users/history coffe/add new user

--- ifsc-bcd/2024.09.20/app/src/main/java/org/example/Cafeteira.java
+++ ifsc-bcd/2024.09.20/app/src/main/java/org/example/Cafeteira.java
@@ -20,7 +20,6 @@ public class Cafeteira {
             FileWriter arquivo = new FileWriter(filename, append);
             for (List<String> elem : linhas) {
                 arquivo.append(String.join(",", elem));
-                arquivo.append("\n");
             }
             arquivo.flush();
             arquivo.close();
@@ -55,27 +54,39 @@ public class Cafeteira {
     public static String userInfo(int id) {
         ArrayList<ArrayList<String>> linhasUsuarios = le("usuarios.csv");
         for(int i = 0; i < linhasUsuarios.size(); i++) {
-            System.out.println();
             if(linhasUsuarios.get(i).get(0).equals(String.valueOf(id))) {
                 return "\nInformações do usuário id " + id + "\n Nome: " + linhasUsuarios.get(i).get(1) + "\n Email: " + linhasUsuarios.get(i).get(2) + "\n Cŕedito dísponivel: R$" + linhasUsuarios.get(i).get(3);
             }
+            return "\n Usuário não encontrado!";
         }
         String resp = "\tInformações do usuário id " + id;
         return resp;
     }
 
     public static String hist(int id) {
-        String resp = "\tHistórico de cafés do usuário id " + id;
-
-        return resp;
+        ArrayList<ArrayList<String>> historicoCafeteira = le("consumo.csv");
+        String resp = "";
+        for(int i = 0; i < historicoCafeteira.size(); i++){
+            if(historicoCafeteira.get(i).get(0).equals(String.valueOf(id))){
+                resp += "O usuário de id: " + id + " consumiu um café do tipo " + historicoCafeteira.get(i).get(1) + " no dia " + historicoCafeteira.get(i).get(2) + " as " + historicoCafeteira.get(i).get(3) + " horas \n";
+            }
+        }
+        if (!resp.equals("")){
+            return resp;
+        }
+        return "O usuário informado ainda não consumiu nenhum dos nossos cafés!";
     }
 
     public static String cafInfo() {
         ArrayList<ArrayList<String>> linhaCafeteira = le("cafeteira.csv");
-        return "O máximo de cafés que é suportado: " + linhaCafeteira.get(0) + "\n Quantidade de cafés disponives: " + linhaCafeteira.get(1) + "\n Valor da cápsula: " + linhaCafeteira.get(2);
+        return "O máximo de cafés que é suportado: " + linhaCafeteira.get(0).get(0) + " cápsulas;" + "\nQuantidade de cafés disponives: " + linhaCafeteira.get(0).get(1) + " cápsulas;" +  "\nValor da cápsula: R$" + linhaCafeteira.get(0).get(2) + ";";
     }
 
-    public static String userAdd(int id) {
+    public static String userAdd(String id, String nome, String email, String credito) {
+        List<List<String>> linhas = new ArrayList<>();
+        linhas.add(Arrays.asList(id,nome,email,credito));
+        escreve(linhas,"usuarios.csv",true);
+
         String resp = "\tUsuário de id " + id + " adicionado";
 
         return resp;
@@ -127,33 +138,45 @@ public class Cafeteira {
             System.out.println("Digite 8: Para sair");
             System.out.print("Sua opção: ");
             opcao = in.nextInt();
+            in.nextLine();
 
             if (opcao == 1) {
                 System.out.print("Entre com o id do usuário: ");
                 id = in.nextInt();
                 String resp = userInfo(id);
                 System.out.println(resp);
+                System.out.println();
             } else if (opcao == 2) {
                 System.out.print("Entre com o id do usuário: ");
                 id = in.nextInt();
+                System.out.println();
                 String resp = hist(id);
                 System.out.println(resp);
             } else if (opcao == 3) {
                 System.out.println("Informações da cafeteira:");
                 String resp = cafInfo();
                 System.out.println(resp);
+                System.out.println();
             } else if (opcao == 4) {
                 System.out.println("Adicionando novo usuário:");
                 System.out.print("Entre com o id do usuário: ");
-                id = in.nextInt();
-                String resp = userAdd(id);
+                String idUsuario = in.nextLine();
+                System.out.print("Entre com o nome do usuário: ");
+                String nome = in.nextLine();
+                System.out.print("Entre com o email do usuário: ");
+                String email = in.nextLine();
+                System.out.print("Entre com o crédito do " + nome +": R$ ");
+                String credito = in.nextLine();
+                String resp = userAdd(idUsuario, nome, email, credito);
                 System.out.println(resp);
+                System.out.println();
             } else if (opcao == 5) {
                 System.out.println("Removendo usuário:");
                 System.out.print("Entre com o id do usuário: ");
                 id = in.nextInt();
                 String resp = remUser(id);
                 System.out.println(resp);
+                System.out.println();
             } else if (opcao == 6) {
                 System.out.println("Servindo café:");
                 System.out.print("Entre com o id do usuário: ");
@@ -162,9 +185,11 @@ public class Cafeteira {
                 tipo = in.nextInt();
                 String resp = servir(id, tipo);
                 System.out.println(resp);
+                System.out.println();
             } else if (opcao == 7) {
                 String resp = reabastecer();
                 System.out.println(resp);
+                System.out.println();
             } else if (opcao == 8) {
                 continua = false;
             }
