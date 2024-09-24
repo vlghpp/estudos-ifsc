a0a59d1a77c01bb06b92d644f878376dd6dd7914
Aluno <henriquepworking@gmail.com>
2024-09-24 20:09:52 -0300

    update: new exercise FilaExpansiva

---
+++ ifsc-esd/2024.09.24/app/src/main/java/org/example/Fila.java
@@ -0,0 +1,72 @@
+package org.example;
+
+public class Fila {
+    Object[] area;
+    int inicio, fim;
+    int n; // quantidade de valores armazenados
+
+    public Fila(int quantidade) {
+        area = new Object[quantidade];
+        this.inicio = 0;
+        this.n = 0;
+        this.fim = 0;
+    }
+
+    public int length() {
+        return this.n;
+    }
+
+    public int capacidade() {
+        return this.fim - this.inicio;
+    }
+
+    public void adiciona(Object valor){
+        if(!estaCheia()){
+            if(fim == area.length){
+                fim = 0;
+            }
+            area[fim] = valor;
+            fim++;
+            n++;
+        }else{
+            throw new IndexOutOfBoundsException("FILA TA CHEIA!");
+        }
+    }
+
+    public Object remove(){
+        if(!estaVazia()){
+            if(fim == area.length){
+                fim = 0;
+            }
+
+            Object elemento = area[inicio];
+            area[inicio] = null;
+            n--;
+            inicio = (inicio + 1) % area.length;
+            return elemento;
+        }else{
+            throw new IndexOutOfBoundsException("FILA TA CHEIA!");
+        }
+    }
+
+    public Object frente() {
+        if(!estaVazia()){
+            return area[inicio];
+        }else{
+            throw new IndexOutOfBoundsException("FILA TA CHEIA!");
+        }
+    }
+
+    public boolean estaVazia(){
+        return n==0;
+    }
+
+    public boolean estaCheia(){
+        return n == area.length;
+    }
+    public void limpa() {
+        for (int i = 0; i < area.length; i++) {
+            area[i] = null;
+        }//area = new Object[];
+    }
+}
---
+++ ifsc-esd/2024.09.24/app/src/main/java/org/example/FilaExpansiva.java
@@ -0,0 +1,9 @@
+
+package org.example;
+
+public class FilaExpansiva {
+
+    public static void main(String[] args) {
+
+    }
+}
