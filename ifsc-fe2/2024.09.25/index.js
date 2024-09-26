dc17ffff7f8862749224f80887600382d1364abd
Aluno <henriquepworking@gmail.com>
2024-09-25 21:48:27 -0300

    update: add exercise calculator

---
+++ ifsc-fe2/2024.09.25/index.js
@@ -0,0 +1,14 @@
+const numeroVisor = document.getElementById('visor')
+
+const soma = document.getElementById('soma')
+const sub = document.getElementById('subtracao')
+const mult = document.getElementById('multiplicacao')
+const divisao = document.getElementById('divisao')
+const poten = document.getElementById('potenciacao')
+const raiz = document.getElementById('raiz')
+const porcen = document.getElementById('porcentagem')
+
+
+const um = document.getElementById('um')
+
+
