6015ae1f1d70c4e058f940c5950d68a22a3628c8
Aluno <henriquepworking@gmail.com>
2024-09-27 21:14:10 -0300

    feat: new functions to calculator

--- ifsc-fe2/2024.09.25/index.js
+++ ifsc-fe2/2024.09.25/index.js
@@ -8,15 +8,56 @@ const poten = document.getElementById('potenciacao')
 const raiz = document.getElementById('raiz')
 const porcen = document.getElementById('porcentagem')
 
+let valorDisplay = ''
+let historico = []
+let x = 0
+let y = 0
+let operador = ''
 
-const numeros = document.querySelectorAll('.numero')
 
-numeros.forEach(numero => {
-    numero.addEventListener('click', e => {
-        const texto = numero.innerHTML
-        numeroVisor.innerHTML =  
+function atualizaDisplay(){
+    document.querySelector('#display').textContent = valorDisplay || '0'
+}
+
+function limpaDisplay() {
+    valorDisplay = ''
+    atualizaDisplay()
+}
+
+function adicionaSimbolo(simbolo){
+    valorDisplay += simbolo
+    atualizaDisplay()
+}
+
+function adicionaOperador(simbolo){
+    x = parseFloat(valorDisplay)
+    operador = simbolo
+    valorDisplay += simbolo
+    atualizaDisplay()
+}
+
+function calculaResultado(){
+    y = parseFloat(valorDisplay)
+    switch(operador){
+        case "+":
+            valorDisplay = x + y
+            break
+
+
+        default:
+            break
+    }
+}
+
+document.querySelectorAll('.numero').forEach(numero => {
+    numero.addEventListener('click', () => {
+        adicionaSimbolo(numero.textContent)
     })
 })
 
+
+document.querySelector('#soma').addEventListener('click', () => {
+    adicionaSimbolo("+")
+})
 
 
