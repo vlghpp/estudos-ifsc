0aec21708c374809964bdd04a7a7a9fd666a062d
Aluno <henriquepworking@gmail.com>
2024-10-23 22:26:58 -0300

    update: add exercises of day 2024.10.16

---
+++ ifsc-fe2/2024.10.23/exercicios/exercicio01.js
@@ -0,0 +1,18 @@
+let numeros = [0,[1,1,[2,3], 5], 8, 13, 21]
+
+function somaArray(array){
+    let soma = 0
+
+    for (let i = 0; i < array.length; i++) {
+        if(Array.isArray(array[i])){
+            soma += somaArray(array[i])
+        }else if (typeof array[i] === 'number'){
+            soma += array[i]
+        }
+
+    }
+    return soma
+}
+
+
+console.log(somaArray(numeros))
