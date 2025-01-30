8813f00ab870343df289f461c9ffad650d285adf
Aluno <henriquepworking@gmail.com>
2025-01-29 22:19:54 -0300

    update: add content about react, exercise recipes(receitas)

---
+++ ifsc-fe2/2025.01.29/receitas/src/App.jsx
@@ -0,0 +1,59 @@
+import './App.css'
+import Header from './components/Header'
+import Recipe from './components/Recipe'
+
+function App() {
+  const recipes = [{
+      nome: "Salmão Assado",
+      ingredientes: [
+          { nome: "Salmão", quantidade: 1, medida: "grande" },
+          { nome: "Pinoli", quantidade: 1, medida: "xícara" },
+          { nome: "Alface Manteiga", quantidade: 2, medida: "xícaras" },
+          { nome: "Abobrinha Amarela", quantidade: 1, medida: "média" },
+          { nome: "Azeite de Oliva", quantidade: 0.5, medida: "xícara" },
+          { nome: "Alho", quantidade: 3, medida: "dentes" },
+      ],
+      instrucoes: [
+          "Preaqueça o forno a 180°C.",
+          "Espalhe o azeite de oliva em uma assadeira de vidro.",
+          "Adicione a abobrinha amarela e leve ao forno por 30 minutos.",
+          "Adicione o salmão, o alho e os pinolis à assadeira.",
+          "Asse por 15 minutos.",
+          "Retire do forno. Adicione a alface e sirva.",
+      ],
+  },
+  {
+      nome: "Tacos de Peixe",
+      ingredientes: [
+          { nome: "Peixe Branco", quantidade: 1, medida: "grande" },
+          { nome: "Queijo", quantidade: 1, medida: "xícara" },
+          { nome: "Alface Americana", quantidade: 2, medida: "xícaras" },
+          { nome: "Tomates", quantidade: 2, medida: "grandes" },
+          { nome: "Tortilhas", quantidade: 3, medida: "médias" },
+      ],
+      instrucoes: [
+          "Grelhe o peixe até que esteja completamente cozido.",
+          "Coloque o peixe sobre as 3 tortilhas.",
+          "Cubra com alface, tomates e queijo.",
+      ]
+  }]
+
+
+  return (
+    <>
+      <Header />
+      <main>
+        {recipes.map((recipe, i)=> {
+          return <Recipe
+          key = {i}
+          name = {recipe.nome}
+          ingredientes = {recipe.ingredientes}
+          instructions={recipe.instrucoes}
+          />
+        })}
+      </main>
+    </>
+  )
+}
+
+export default App
